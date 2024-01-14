89cd3df39956a19a1059a2295ef1d8650e6cfeac
sanipop <thepersian82@gmail.com>
2024-01-13 19:53:44 -0800

    :chmoded

---
+++ file_storage.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Script for the filestorage class."""
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
+
+class FileStorage:
+    """Class defination of the file storage.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """the dictionary cointainer of the class."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """define a new object <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+
+    def save(self):
+        """function to save i JSON."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
+
+    def reload(self):
+        """Function to create the obj from Json."""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
